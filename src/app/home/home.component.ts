5704095ea4019304d3f3ccf76ef37d0bbf7065f7
DUCLOS Maxime <mduclos@sodifrance.fr>
2020-10-27 20:30:50 +0100

    Refactoring pour comparaison entre deux dates.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import {HospitaliseService} from '../services/hospitalise.service';
 import {Subscription} from 'rxjs';
 import * as moment from 'moment';
 import {UIChart} from 'primeng/chart';
+import {fr} from '../services/local';
 
 @Component({
   selector: 'ass-home',
@@ -15,12 +16,12 @@ export class HomeComponent implements OnInit {
   chart: UIChart;
   subscription: Subscription;
   hospitaliseParJour = [];
-  hospitaliseParAge = [];
-  hospitaliseReaParAge = [];
   minDate;
   maxDate;
   jour;
+  jour2;
   data: any;
+  fr = fr;
 
   ngOnInit(): void {
     this.minDate = new Date();
@@ -34,28 +35,17 @@ export class HomeComponent implements OnInit {
     this.subscription = this.newsService.getCsv().subscribe(csv => {
       if (csv[2].data.length > 0 && this.hospitaliseParJour.length === 0) {
         this.hospitaliseParJour = csv[2].data.reduce((r, v, i, a, k = v.jour) => ((r[k] || (r[k] = [])).push(v), r), {});
+        const maxDatePossible = Object.entries(this.hospitaliseParJour)[Object.entries(this.hospitaliseParJour).length - 2][0];
+        this.maxDate = moment(maxDatePossible, 'YYYY-MM-DD').toDate();
       }
     });
   }
 
   updateChart(): any {
-    this.hospitaliseParAge = [];
-    this.hospitaliseReaParAge = [];
     this.chart.reinit();
     this.chart.refresh();
-
     const date = moment(this.jour).format('YYYY-MM-DD');
     const dateFr = moment(this.jour).format('DD-MM-YYYY');
-    // tslint:disable-next-line:radix
-    const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue);
-    Object.entries(this.hospitaliseParJour[date]
-      .reduce((r, v, i, a, k = v.cl_age90) => ((r[k] || (r[k] = [])).push(v.hosp) , r), {}))
-      .map((ha: any) => this.hospitaliseParAge.push(ha['1'].reduce(reducer)));
-
-    Object.entries(this.hospitaliseParJour[date]
-      .reduce((r, v, i, a, k = v.cl_age90) => ((r[k] || (r[k] = [])).push(v.rea) , r), {}))
-      .map((ha: any) => this.hospitaliseReaParAge.push(ha['1'].reduce(reducer)));
-
     this.data = {
       labels: ['0- 9', '10 - 19', '20 - 29', '30 - 39', '40 - 49', '50 - 59', '60 - 69', '70 - 79', '80 - 89', '>90' ],
       datasets: [
@@ -63,15 +53,45 @@ export class HomeComponent implements OnInit {
           label: `Patient covid hospitaliser au ${dateFr}`,
           backgroundColor: '#42A5F5',
           borderColor: '#1E88E5',
-          data: this.hospitaliseParAge.slice(1)
+          data: this.gethospitaliseByFilterAndDate('hosp', date)
         },
         {
           label: `Patient covid hospitaliser en réanimation au ${dateFr}`,
           backgroundColor: '#9CCC65',
           borderColor: '#7CB342',
-          data: this.hospitaliseReaParAge.slice(1)
+          data: this.gethospitaliseByFilterAndDate('rea', date)
         }
       ]
     };
+    if(this.jour2) {
+      this.updateChartJour2();
+    }
+  }
+
+  updateChartJour2(): any {
+    const date = moment(this.jour2).format('YYYY-MM-DD');
+    const dateFr = moment(this.jour2).format('DD-MM-YYYY');
+    this.data.datasets.push({
+      label: `Patient covid hospitaliser au ${dateFr}`,
+      backgroundColor: '#9f0650',
+      borderColor: '#980235',
+      data: this.gethospitaliseByFilterAndDate('hosp', date)
+    });
+    this.data.datasets.push({
+      label: `Patient covid hospitaliser au ${dateFr}`,
+      backgroundColor: '#b80000',
+      borderColor: '#ea0000',
+      data: this.gethospitaliseByFilterAndDate('rea', date)
+    });
+  }
+
+  gethospitaliseByFilterAndDate(filtre: string, date: string): any[] {
+    const hospitalise = [];
+    // tslint:disable-next-line:radix
+    const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue);
+    Object.entries(this.hospitaliseParJour[date]
+      .reduce((r, v, i, a, k = v.cl_age90) => ((r[k] || (r[k] = [])).push(v[filtre]) , r), {}))
+      .map((ha: any) => hospitalise.push(ha['1'].reduce(reducer)));
+    return hospitalise.slice(1);
   }
 }
---
+++ src/app/services/local.ts
@@ -0,0 +1,12 @@
+export const fr = {
+  firstDayOfWeek: 0,
+  dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+  dayNamesShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+  dayNamesMin: ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa'],
+  monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+  monthNamesShort: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Jun', 'Jui', 'Aoû', 'Sep', 'Oct', 'Nov', 'Dec'],
+  today: 'Aujourd\'hui',
+  clear: 'Effacer',
+  dateFormat: 'dd/mm/yy',
+  weekHeader: 'Semaine'
+};
