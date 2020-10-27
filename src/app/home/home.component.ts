32706ff7ffe27aa005a76eb33f269adb6ca291f4
DUCLOS Maxime <mduclos@sodifrance.fr>
2020-10-27 21:39:37 +0100

    Correction faute ortho

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -50,13 +50,13 @@ export class HomeComponent implements OnInit {
       labels: ['0- 9', '10 - 19', '20 - 29', '30 - 39', '40 - 49', '50 - 59', '60 - 69', '70 - 79', '80 - 89', '>90' ],
       datasets: [
         {
-          label: `Patient covid hospitaliser au ${dateFr}`,
+          label: `Patients covid hospitalisés au ${dateFr}`,
           backgroundColor: '#42A5F5',
           borderColor: '#1E88E5',
           data: this.gethospitaliseByFilterAndDate('hosp', date)
         },
         {
-          label: `Patient covid hospitaliser en réanimation au ${dateFr}`,
+          label: `Patients covid hospitalisés en réanimation au ${dateFr}`,
           backgroundColor: '#9CCC65',
           borderColor: '#7CB342',
           data: this.gethospitaliseByFilterAndDate('rea', date)
@@ -72,13 +72,13 @@ export class HomeComponent implements OnInit {
     const date = moment(this.jour2).format('YYYY-MM-DD');
     const dateFr = moment(this.jour2).format('DD-MM-YYYY');
     this.data.datasets.push({
-      label: `Patient covid hospitaliser au ${dateFr}`,
+      label: `Patients covid hospitalisés au ${dateFr}`,
       backgroundColor: '#9f0650',
       borderColor: '#980235',
       data: this.gethospitaliseByFilterAndDate('hosp', date)
     });
     this.data.datasets.push({
-      label: `Patient covid hospitaliser au ${dateFr}`,
+      label: `Patients covid hospitalisés au ${dateFr}`,
       backgroundColor: '#b80000',
       borderColor: '#ea0000',
       data: this.gethospitaliseByFilterAndDate('rea', date)
